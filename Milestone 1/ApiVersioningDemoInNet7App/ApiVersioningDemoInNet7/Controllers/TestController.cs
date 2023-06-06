444cf428db9a2d8613ff99cc64427c515ffba160
Dipendu Paul <dipendupaul@gmail.com>
2023-06-06 13:08:26 +0530

    initial commit

---
+++ Milestone 1/ApiVersioningDemoInNet7App/ApiVersioningDemoInNet7/Controllers/TestController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiVersioningDemoInNet7.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet, Route("testmethod")]
+        public string TestMethod()
+        {
+            return "Test method call successful";
+        }
+    }
+}
---
+++ Milestone 1/ApiVersioningDemoInNet7App/ApiVersioningDemoInNet7/Program.cs
@@ -0,0 +1,19 @@
+
+using ApiVersioningDemoInNet7;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var host = CreateHostBuilder(args).Build();
+        host.Run();
+
+    }
+
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+}
